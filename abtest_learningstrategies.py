75dbea095735faa70a6b691ffc5a9bec9664121d
CogLab Research assistants <researchassistants@cognitivescience.co>
2013-11-18 16:50:49 -0800

    finish implementing all conditions of ab test

--- abtest_learningstrategies.py
+++ abtest_learningstrategies.py
@@ -102,31 +102,31 @@ growth_messages = [
 
 #the what why how study strategy
 wwh_strat = ('<a href="#" class="show-subhint" data-subhint="what-why-how">'
-                              'Click here to learn about the'
-                              ' "<span class="hint_purple" style="font-weight:bold">What? Why? How?</span> strategy</a>'
-                              '<div class="subhint" id="what-why-how">'
-                              'To use this strategy, ask yourself these'
-                              ' "<span class="hint_purple" style="font-weight: bold">'
-                              'What? Why? How?</span>"'
-                              ' questions after each hint in a problem.'
-                              '<br><span class="hint_purple" style="font-weight: bold">'
-                              'What does this step mean to you?</span>'
-                              '<br>'
-                              '<span class="hint_purple" style="font-weight: bold">'
-                              'Why is it helpful to take this step?</span>'
-                              '<br><span class="hint_purple" style="font-weight: bold">'
-                              'How do you know this step is right?</span>'
-                              '<br>'
-                              '<br><br>'
-                              '<a href="#" class="show-subhint" data-subhint="encouragement" data-hidden-text="Hide Information">'
-                              'What if I can’t do it?</a>'
-                              '<div class="subhint" id="encouragement">'
-                              'Many students are not sure what to say, or think '
-                              'their answer isn’t good. That is fine, as long as you'
-                              '<span style="font-weight: bold; font-style:italic">'
-                              ' try</span>'
-                              ' to think about the question, by typing or saying the answer to yourself.'
-                              '</div></div>')
+             'Click here to learn about the'
+             ' "<span class="hint_purple" style="font-weight:bold">What? Why? How?</span> strategy</a>'
+             '<div class="subhint" id="what-why-how">'
+             'To use this strategy, ask yourself these'
+             ' "<span class="hint_purple" style="font-weight: bold">'
+             'What? Why? How?</span>"'
+             ' questions after each hint in a problem.'
+             '<br><span class="hint_purple" style="font-weight: bold">'
+             'What does this step mean to you?</span>'
+             '<br>'
+             '<span class="hint_purple" style="font-weight: bold">'
+             'Why is it helpful to take this step?</span>'
+             '<br><span class="hint_purple" style="font-weight: bold">'
+             'How do you know this step is right?</span>'
+             '<br>'
+             '<br><br>'
+             '<a href="#" class="show-subhint" data-subhint="encouragement" data-hidden-text="Hide Information">'
+             'What if I can’t do it?</a>'
+             '<div class="subhint" id="encouragement">'
+             'Many students are not sure what to say, or think '
+             'their answer isn’t good. That is fine, as long as you'
+             '<span style="font-weight: bold; font-style:italic">'
+             ' try</span>'
+             ' to think about the question, by typing or saying the answer to yourself.'
+             '</div></div>')
 
 
 
@@ -159,16 +159,28 @@ def add_header_text_to_cards(card, user_exercise):
         
     elif test_condition == "header":
         test_subcondition = experiments.CoreMetrics.ab_test(
-            "learning support subtest",
+            "header type subtest",
             alternative_params={
                 "mindset only": 1, # mindset message, exactly as in previous expt            
                 "explanation only": 1, #provides dropdown explanation strategy
                 "mindset + explanation": 1}, #provides a combination
             core_categories='all')
         test_condition += "." + test_subcondition
-        message = random.choice(mindset_messages)
-        card.growthHeader = "<p><em>" + message + "</em></p>"
         
+
+        if test_condition == "header.mindset only"
+            message = random.choice(mindset_messages)
+            card.growthHeader = "<p><em>" + message + "</em></p>" #show mindset message
+
+
+        if test_condition == "header.explanation only"
+            card.growthHeader = wwh_strat #show explanation strategy
+        
+        if test_condition == "header.mindset + explanation"
+            message = random.choice(mindset_messages)
+            card.growthHeader = wwh_strat + "<p><em>" + message + "</em></p>" #show strategy, followed by mindset message
+
+
     elif test_condition == "learning support":
         # STOPSHIP. People in the "learning support" condition are assigned to 1 of 2 sub-conditions.
         # "webpage link" provides a link to a webpage on KA with the LearningCoach study information (like brain workout page in mindset study).
